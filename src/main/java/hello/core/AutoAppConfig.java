6ab587fc67ee0a1d92e1adfdc851e88ed4fee8be
seulGit <seulgi9421@gmail.com>
2024-04-19 16:50:49 +0900

    240419 - 탐색위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AutoAppConfig.class, 지정을 안하면 해당 클래스가 속한 패키지가 시작위치가 된다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
